da70b244d8a93b77e5e4636c18e918b08892b628
zvarychkat1 <neridova13@gmail.com>
2020-04-15 21:10:20 +0300

    todo form

---
+++ src/components/TodoForm.js
@@ -0,0 +1,20 @@
+import React, {useState} from "react";
+
+function TodoForm() {
+    const [todo, setTodo] = useState({
+        id: "",
+        task:"",
+        completed: false
+    })
+
+    return (
+        <form>
+            <input />
+            <button />
+        </form>
+    )
+}
+
+
+
+export default TodoForm
